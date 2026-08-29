e2dbdec95b1bfa13caa25a67bcc00acee5dfdebe
SNelectric 2026 <snelectric2026@gmail.com>
2026-08-29 13:37:51 +0300

    Update service worker registration path

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js', { scope: '/' })
+    navigator.serviceWorker.register('./sw.js')
       .then((reg) => console.log('[PWA] تم تسجيل Service Worker بنجاح:', reg))
       .catch((err) => console.warn('[PWA] فشل تسجيل Service Worker:', err));
   });
