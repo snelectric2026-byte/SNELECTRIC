57cd98153d6429be7f6534cfc6e3c0a1ee2a62a7
SNelectric 2026 <snelectric2026@gmail.com>
2026-08-29 17:37:58 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,6 @@
-// js/config.js
-window.SNELECTRIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkaGNvb2h1b2ZlcnBvYmNjcHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1NDg1MzMsImV4cCI6MjA1NDEyNDUzM30.F_Gg058-AThs33e_v3o0oW0e';
-window.SNELECTRIC_ADMIN_PHONE = '201287837118';
+// إعدادات الاتصال بـ Supabase الخاصة بمشروع SN ELECTRIC
+const SUPABASE_URL = "https://kdhcoohuoferpobccpwo.supabase.co";
+const SUPABASE_ANON_KEY = "EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkaGNvb2h1b2ZlcnBvYmNjcHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3OTc4MDYsImV4cCI6MjEwMDM3MzgwNn0.fyNmGIH-7G1I79TzBZN1d_akborKFgsJFAdDuDt72E8";
+
+// تهيئة العميل للاتصال بقاعدة البيانات
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
