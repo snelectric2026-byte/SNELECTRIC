5acd2d6097628cd40864f68318ce8ed5645465f7
SNelectric 2026 <snelectric2026@gmail.com>
2026-08-29 17:40:00 +0300

    Change Supabase_ANON_KEY value
    Updated the Supabase anonymous key in config.js.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // إعدادات الاتصال بـ Supabase الخاصة بمشروع SN ELECTRIC
 const SUPABASE_URL = "https://kdhcoohuoferpobccpwo.supabase.co";
-const SUPABASE_ANON_KEY = "EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkaGNvb2h1b2ZlcnBvYmNjcHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3OTc4MDYsImV4cCI6MjEwMDM3MzgwNn0.fyNmGIH-7G1I79TzBZN1d_akborKFgsJFAdDuDt72E8";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkaGNvb2h1b2ZlcnBvYmNjcHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3OTc4MDYsImV4cCI6MjEwMDM3MzgwNn0.fyNmGIH-7G1I79TzBZN1d_akborKFgsJFAdDuDt72E8";
 
 // تهيئة العميل للاتصال بقاعدة البيانات
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
